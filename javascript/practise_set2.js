9355f97638b4b97a25eeb5869ba16644045b11ac
shivanshiyadav <143997818+shivanshiyadav@users.noreply.github.com>
2023-11-02 22:40:35 +0530

    js

--- javascript/practise_set2.js
+++ javascript/practise_set2.js
@@ -1,8 +1,36 @@
-let age = prompt("what is ur age");
-for(age>10 && age<20)
-{
-    console.log("your age lie btween 10 and 20"); 
-};
-else{
-    console.log(" you are not elegible");
+// let age = prompt("what is ur age");
+// for(age>10 && age<20)
+// {
+//     console.log("your age lie btween 10 and 20"); 
+// };
+// else{
+//     console.log(" you are not elegible");
+// }
+
+
+// let num =24;
+// // num= Num.parseInt[age]
+// if(num % 2 ==0 && num %3 == 0){
+//   console.log("your number is divisible by 2 and 3")
+
+// }
+// else{
+//   console.log("number is not divisible by 2and 3");
+// }
+//problem
+const age =12;
+// console.log("what is your age");
+// age = Number.parseInt(age)
+switch (age){
+    case ('12'):
+        console.log("ur age is 12");
+        break;
+    case ('13'):
+        console.log("ur age is 13");
+        break;
+    case ('14'):
+        console.log("ur age is 14");
+        break;
+        default:
+            console.log("ur not special ");
 }
