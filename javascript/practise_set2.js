49eafa7635ba200a21703ad94376ec6fb6f68cde
shivanshiyadav <143997818+shivanshiyadav@users.noreply.github.com>
2023-11-05 00:12:46 +0530

    js

--- javascript/practise_set2.js
+++ javascript/practise_set2.js
@@ -1,51 +1,51 @@
-let age1 =10;
-if(age1>10 && age1<20)
-{
-  console.log("your age lie btween 10 and 20"); 
-}
-else{
-    console.log(" you are not elegible");
-}
+// let age1 =10;
+// if(age1>10 && age1<20)
+// {
+//   console.log("your age lie btween 10 and 20"); 
+// }
+// else{
+//     console.log(" you are not elegible");
+// }
 
-console.log('------------------');
+// console.log('------------------');
 
-let num =24;
-// num= Num.parseInt[age]
-if(num % 2 ==0 && num %3 == 0){
-  console.log("your number is divisible by 2 and 3")
+// let num =24;
+// // num= Num.parseInt[age]
+// if(num % 2 ==0 && num %3 == 0){
+//   console.log("your number is divisible by 2 and 3")
 
-}
-else{
-  console.log("number is not divisible by 2and 3");
-}
-console.log('-------------------');
- let age ='12';
-// console.log("what is your age");
-// age = Number.parseInt(age)
-switch (age){
-    case ('12'):
-        console.log("ur age is 12");
-        break;
-    case ('13'):
-        console.log("ur age is 13");
-        break;
-    case ('14'):
-        console.log("ur age is 14");
-        break;
-        default:
-            console.log("ur not special ");
-}
-console.log('-------------------');
+// }
+// else{
+//   console.log("number is not divisible by 2and 3");
+// }
+// console.log('-------------------');
+//  let age ='12';
+// // console.log("what is your age");
+// // age = Number.parseInt(age)
+// switch (age){
+//     case ('12'):
+//         console.log("ur age is 12");
+//         break;
+//     case ('13'):
+//         console.log("ur age is 13");
+//         break;
+//     case ('14'):
+//         console.log("ur age is 14");
+//         break;
+//         default:
+//             console.log("ur not special ");
+// }
+// console.log('-------------------');
 
-const expr = 'papayas';
-switch(expr){
-    case 'oranges':
-        console.log("oranges 5 $")
-        break;
-    case 'papayas':
-        case 'oranges':
-        console.log("papaya 5 $,oranges $8"  )
-        break;
-        default:
-        console.log("out of stock")
-    }
+// const expr = 'papayas';
+// switch(expr){
+//     case 'oranges':
+//         console.log("oranges 5 $")
+//         break;
+//     case 'papayas':
+//         case 'oranges':
+//         console.log("papaya 5 $,oranges $8"  )
+//         break;
+//         default:
+//         console.log("out of stock")
+//     }
